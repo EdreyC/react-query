3aaae637e6ce064c20fc6d33f5c511f8e9d8456b
EdreyC <edreycarneiro.profissional@gmail.com>
2022-03-07 10:02:17 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import useFetch from './hooks/useFetch';
+import { useQuery } from "react-query";
+import axios from 'axios';
+
+type Repository = {
+  full_name: string;
+  description:string
+}
+
+function App() {
+  const {data, isFetching} = useQuery<Repository[]>('repos',async()=>{
+    const response = await axios.get('https://api.github.com/users/EdreyC/repos')
+    return response.data
+  })
+  return (
+    <div className="App">
+      <ul> 
+        {isFetching && <p>Carregando...</p>}
+        {data?.map(repo=>{
+          return(
+            <li key={repo.full_name}>
+              <strong>{repo.full_name}</strong>
+              <p>{repo.description}</p>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default App
